c0ab90e17b1a51ce598ba114aee902546955edee
sakamoto.kazuki <sakamoto.kazuki@4033c2de-e5d5-11dd-ab07-b19b4dad1b3d>
2009-01-25 13:26:12 +0000

    add LOCAL_MODULE_TAGS

--- trunk/irbapp/Android.mk
+++ trunk/irbapp/Android.mk
@@ -1,6 +1,7 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
+LOCAL_MODULE_TAGS:= eng development
 LOCAL_PACKAGE_NAME:= irbapp
 LOCAL_SRC_FILES:= $(call all-java-files-under, src)
 LOCAL_SDK_VERSION:= current
