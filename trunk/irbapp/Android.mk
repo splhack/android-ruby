b667105f1b9cae45dedda92c84ebfd3c7af57523
sakamoto.kazuki <sakamoto.kazuki@4033c2de-e5d5-11dd-ab07-b19b4dad1b3d>
2009-04-29 12:32:52 +0000

    LOCAL_JNI_SHARED_LIBRARIES:= libruby_jni libruby

--- trunk/irbapp/Android.mk
+++ trunk/irbapp/Android.mk
@@ -5,5 +5,6 @@ LOCAL_MODULE_TAGS:= eng development
 LOCAL_PACKAGE_NAME:= irbapp
 LOCAL_SRC_FILES:= $(call all-java-files-under, src)
 LOCAL_SDK_VERSION:= current
+LOCAL_JNI_SHARED_LIBRARIES:= libruby_jni libruby
 
 include $(BUILD_PACKAGE)
